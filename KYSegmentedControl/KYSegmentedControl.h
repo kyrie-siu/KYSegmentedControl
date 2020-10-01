ff694d8fb9c500545bcc92ad44757e1f9e383556
Kyrie SIU <kyrie.siu@gmail.com>
2020-10-01 19:09:21 +0800

    Init commit

---
+++ KYSegmentedControl/KYSegmentedControl.h
@@ -0,0 +1,18 @@
+//
+//  KYSegmentedControl.h
+//  KYSegmentedControl
+//
+//  Created by SIU Suet Long on 1/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KYSegmentedControl.
+FOUNDATION_EXPORT double KYSegmentedControlVersionNumber;
+
+//! Project version string for KYSegmentedControl.
+FOUNDATION_EXPORT const unsigned char KYSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KYSegmentedControl/PublicHeader.h>
+
+
